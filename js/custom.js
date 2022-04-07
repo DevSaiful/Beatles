c9a53a2b245561d2430f1bc39ca3d38b6aa00198
DevSaiful <webdesigner.saiful@gmail.com>
2022-04-08 00:36:06 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+
+    if (scrolling > 100) {
+        $('.navbar').addClass('sticky_header');
+    } else {
+        $('.navbar').removeClass('sticky_header');
+    }
+});
+
